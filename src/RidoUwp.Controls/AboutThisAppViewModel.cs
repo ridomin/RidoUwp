ef19fe7577c1399a83964fded52e8a554791dbeb
ridomin <ridomin@live.com>
2017-02-25 17:58:22 -0800

    AboutThisApp Control
    Extract version and build metadata

---
+++ src/RidoUwp.Controls/AboutThisAppViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace RidoUwp.Controls
+{
+    public class AboutThisAppViewModel
+    {
+        public Package Package => Package.Current;
+        public BitmapImage AppLogo => new BitmapImage(Package.Current.Logo);
+        public string VersionString => string.Format("{0}.{1}.{2}.{3}", 
+                                            Package.Current.Id.Version.Major, 
+                                            Package.Current.Id.Version.Minor, 
+                                            Package.Current.Id.Version.Build, 
+                                            Package.Current.Id.Version.Revision);
+        public string Metadata
+        {
+            get
+            {
+                XDocument doc = XDocument.Load("AppxManifest.xml", LoadOptions.None);
+                var xname = XNamespace.Get("http://schemas.microsoft.com/developer/appx/2015/build");
+                var metadata = doc.Descendants(xname + "Metadata").First();
+                var res = new StringBuilder();
+                foreach (XElement n in metadata.Elements())
+                {
+                    res.Append(n.Attribute("Name").Value + ":");
+                    res.Append(n.Attribute("Value")?.Value);
+                    res.Append(n.Attribute("Version")?.Value + "\r\n");
+                }
+                return res.ToString();
+            }
+        }
+
+        public string InstalledOn
+        {
+            get
+            {
+                return SafeInstalledOn();
+            }
+        }
+
+        private static string SafeInstalledOn()
+        {
+            DateTimeOffset installed;
+            try
+            {
+                installed = Package.Current.InstalledDate;
+            }
+            catch (Exception)
+            {
+
+                installed = DateTime.Now;
+            }
+            return installed.ToLocalTime().ToString();
+        }
+
+
+    }
+}
